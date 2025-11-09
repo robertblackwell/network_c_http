c7ca55a93af84645bd701d3ac99b2b98de72e95d
Robert Blackwell <rob@whiteacorn.com>
2025-11-10 10:52:27 +1100

    forgot to delete sync_junk for previous commit

--- src/sync_junk/sync_handler_example.h
+++
@@ -1,8 +0,0 @@
-#ifndef c_http_sync_handler_example_h
-#define c_http_sync_handler_example_h
-
-#include <src/http/http_message.h>
-#include <src/sync_junk/sync.h>
-HttpMessageRef app_handler_example(HttpMessageRef request, sync_worker_r wref);
-
-#endif
